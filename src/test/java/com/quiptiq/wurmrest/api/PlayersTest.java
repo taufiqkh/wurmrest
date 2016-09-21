7aac3b8a6562454c708f13e428b1c43609bce9ed
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-09-21 12:32:48 +1000

    Added tests for Players representation

---
+++ src/test/java/com/quiptiq/wurmrest/api/PlayersTest.java
@@ -0,0 +1,83 @@
+package com.quiptiq.wurmrest.api;
+
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.jackson.Jackson;
+import org.junit.Test;
+
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Tests the JSON conversions of the Players representation
+ */
+public class PlayersTest {
+    private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
+
+    private String jsonFixture(String target) {
+        return fixture("fixtures/players/" + target + ".json");
+    }
+
+    @Test
+    public void serializesToJson() throws Exception {
+        final ArrayList<String> names = new ArrayList<>();
+        names.add("Tom");
+        names.add("Dave");
+        Players players = new Players(names);
+        final String expected = MAPPER.writeValueAsString(
+                MAPPER.readValue(jsonFixture("players"), Players.class));
+        assertEquals(expected, MAPPER.writeValueAsString(players));
+    }
+
+    @Test
+    public void serializesCountOnlyToJson() throws Exception {
+        final Players players = new Players(20);
+        final String expected = MAPPER.writeValueAsString(
+                MAPPER.readValue("{\"count\":20}", Players.class));
+        assertEquals(expected, MAPPER.writeValueAsString(players));
+    }
+
+    @Test
+    public void serializesEmptyNames() throws Exception {
+        final Players players = new Players(new ArrayList<>());
+        final String expected = MAPPER.writeValueAsString(
+                MAPPER.readValue(jsonFixture("playersEmpty"), Players.class));
+        assertEquals(expected, MAPPER.writeValueAsString(players));
+    }
+
+    @Test
+    public void deserializesFromJson() throws Exception {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Tom");
+        names.add("Dave");
+        final Players expectedPlayers = new Players(names);
+        Players players = MAPPER.readValue(jsonFixture("players"), Players.class);
+        assertEquals(expectedPlayers.getCount(), players.getCount());
+        assertThat(expectedPlayers.getPlayers(), is(players.getPlayers()));
+    }
+
+    @Test
+    public void deserializesCountOnlyFromJson() throws Exception {
+        final Players expectedPlayers = new Players(10);
+        Players players = MAPPER.readValue("{\"count\":10}", Players.class);
+        assertEquals(expectedPlayers.getCount(), players.getCount());
+        assertNull(players.getPlayers());
+    }
+
+    @Test
+    public void deserializesEmptyNames() throws Exception {
+        final Players expectedPlayers = new Players(new ArrayList<>());
+        Players players = MAPPER.readValue(jsonFixture("playersEmpty"), Players.class);
+        assertEquals(expectedPlayers.getCount(), players.getCount());
+        assertNotNull(players.getPlayers());
+        assertThat(expectedPlayers.getPlayers(), is(players.getPlayers()));
+    }
+
+    @Test(expected = JsonMappingException.class)
+    public void errorsOnMiscountDeserialization() throws Exception {
+        MAPPER.readValue(jsonFixture("playersInvalid"), Players.class);
+    }
+}
